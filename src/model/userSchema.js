4f653c221b7b4827355d4e98732b5dddcf58cb81
whitedevil31 <magz3116@gmail.com>
2020-11-04 03:15:32 +0530

    remove unique

--- src/model/userSchema.js
+++ src/model/userSchema.js
@@ -14,6 +14,7 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      // unique:true
       trim: true,
       validate(value) {
         if (!validator.isEmail(value)) {
