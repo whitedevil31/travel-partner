68991fddda9eae698342aeafce055e3f072dc04c
whitedevil31 <magz3116@gmail.com>
2020-11-04 02:46:02 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -17,15 +17,15 @@ app.use(
 );
 app.use(bodyParser.json());
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "*");
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use(userRoute);
 app.use(travelRoute);
