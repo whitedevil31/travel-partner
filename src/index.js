37ada16304e0da722524fef17ca4bae045cbe240
whitedevil31 <magz3116@gmail.com>
2020-11-04 02:12:53 +0530

    port added

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 
 app.use(userRoute);
 app.use(travelRoute);
-Port = process.env.port || 3000;
-app.listen(Port, () => {
-  console.log(`server is up on port ${Port}`);
+port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("server is up on port " + port);
 });
