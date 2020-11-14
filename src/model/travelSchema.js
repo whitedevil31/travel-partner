ecb05cd900af39fafa109badc6f9c87bf64cc2b6
whitedevil31 <magz3116@gmail.com>
2020-11-14 13:34:45 +0530

    places added

--- src/model/travelSchema.js
+++ src/model/travelSchema.js
@@ -8,6 +8,7 @@ const travelSchema = new mongoose.Schema({
   },
   startDate: { type: Date },
   endDate: { type: Date },
+  places: { type: String },
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Users" },
   ownerName: { type: String, ref: "Users", required: true },
   ownerAge: { type: Number, ref: "Users", required: true },
